0be3a64131e069ab9655ac8b9f1a0b8e6adf2e57
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-01-19 21:59:22 +0300

    Добавлены дополнительные поля

--- app/src/main/java/com/oneseed/weatherapp/screens/MainScreen.kt
+++ app/src/main/java/com/oneseed/weatherapp/screens/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,12 +39,26 @@ fun MainScreen() {
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        text =
-                        SimpleDateFormat("dd MMMM", Locale.getDefault()).format(
+                        text = SimpleDateFormat("dd MMMM", Locale.getDefault()).format(
                             Date()
                         ).toString(),
                         style = TextStyle(fontSize = 15.sp),
                         modifier = Modifier.padding(5.dp)
+                    )
+                    Text(
+                        text = "London",
+                        style = TextStyle(fontSize = 15.sp),
+                        modifier = Modifier.padding(5.dp)
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "4°",
+                        style = TextStyle(fontSize = 50.sp),
+                        modifier = Modifier.padding(5.dp).align(Alignment.CenterVertically)
                     )
                     AsyncImage(
                         model = "https:////cdn.weatherapi.com/weather/64x64/day/113.png",
@@ -51,8 +66,18 @@ fun MainScreen() {
                         modifier = Modifier
                             .padding(5.dp)
                             .size(50.dp)
+                            .align(Alignment.CenterVertically)
                     )
                 }
+                Text(
+                    text = "Облачно",
+                    style = TextStyle(fontSize = 15.sp),
+                )
+                Text(
+                    text = "Ощущается как 4°",
+                    style = TextStyle(fontSize = 15.sp),
+                    modifier = Modifier.padding(bottom = 10.dp)
+                )
             }
         }
     }
