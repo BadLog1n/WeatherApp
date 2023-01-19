9a038ccad94f30d28b6bb82f91837de13cf0b350
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-01-19 21:57:47 +0300

    Реализован SwipeToRefresh

--- app/src/main/java/com/oneseed/weatherapp/screens/MainScreen.kt
+++ app/src/main/java/com/oneseed/weatherapp/screens/MainScreen.kt
@@ -1,19 +1,22 @@
 package com.oneseed.weatherapp.screens
 
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.oneseed.weatherapp.R
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -26,35 +29,104 @@ fun MainScreen() {
             .padding(5.dp)
     ) {
         Card(
-            modifier = Modifier.fillMaxWidth(),
-            elevation = CardDefaults.cardElevation(
+            modifier = Modifier.fillMaxWidth(), elevation = CardDefaults.cardElevation(
                 defaultElevation = 0.dp
-            ),
-            shape = RoundedCornerShape(10.dp)
+            ), shape = RoundedCornerShape(10.dp)
         ) {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text =
-                        SimpleDateFormat("dd MMMM", Locale.getDefault()).format(
-                            Date()
-                        ).toString(),
-                        style = TextStyle(fontSize = 15.sp),
-                        modifier = Modifier.padding(5.dp)
-                    )
-                    AsyncImage(
-                        model = "https:////cdn.weatherapi.com/weather/64x64/day/113.png",
-                        contentDescription = "im2",
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .size(50.dp)
-                    )
+           //create swipe to refresh
+            val state = rememberSwipeRefreshState(isRefreshing = false)
+            SwipeRefresh(
+                state = state,
+                onRefresh = {
+                    state.isRefreshing = true
+                    //simulate network call
+                    GlobalScope.launch {
+                        delay(2000)
+                        state.isRefreshing = false
+                    }
                 }
+            ) {
+                LazyColumn {
+
+                    items(1) {
+
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = SimpleDateFormat("dd MMMM", Locale.getDefault()).format(
+                                    Date()
+                                ).toString(),
+                                style = TextStyle(fontSize = 15.sp),
+                                modifier = Modifier.padding(5.dp)
+                            )
+                            Text(
+                                text = "London",
+                                style = TextStyle(fontSize = 15.sp),
+                                modifier = Modifier.padding(5.dp)
+                            )
+                        }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "4°",
+                                style = TextStyle(fontSize = 50.sp),
+                                modifier = Modifier.padding(5.dp).align(Alignment.CenterVertically)
+                            )
+                            AsyncImage(
+                                model = "https:////cdn.weatherapi.com/weather/64x64/day/113.png",
+                                contentDescription = "im2",
+                                modifier = Modifier
+                                    .padding(5.dp)
+                                    .size(50.dp)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
+                        Text(
+                            text = "Облачно",
+                            style = TextStyle(fontSize = 15.sp),
+                        )
+                        Text(
+                            text = "Ощущается как 4°",
+                            style = TextStyle(fontSize = 15.sp),
+                            modifier = Modifier.padding(bottom = 10.dp)
+                        )
+
+                    }
+                }
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
             }
         }
     }
 }
 
+@Composable
+fun SwipeRefreshCompose() {
+
+    var refreshing by remember { mutableStateOf(false) }
+    LaunchedEffect(refreshing) {
+        if (refreshing) {
+            delay(3000)
+            refreshing = false
+        }
+    }
+
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing = refreshing),
+        onRefresh = { refreshing = true },
+    ) {
+
+        // list view
+
+    }
+
+}
